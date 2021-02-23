43679e161193b067c86213373604eb75aa469573
Maksaud <49394528+Maksaud@users.noreply.github.com>
2021-02-23 21:43:53 +0000

    Now the app is working perfectly through main_raindrops.py

---
+++ main_raindrops.py
@@ -0,0 +1,8 @@
+from run_raindrops import *
+
+# This will run the app allowing user to decide what number to use and print the factorials
+n = ''
+while n != 'exit':
+    n = int(input("Enter a number: "))
+    print_all_factors(n)
+    print(pling_plang_plong(n))

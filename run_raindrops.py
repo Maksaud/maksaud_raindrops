293a3109102e032994841c62e41aaffb4d197aa0
Maksaud <49394528+Maksaud@users.noreply.github.com>
2021-02-23 21:24:39 +0000

    created a testing program to test certain functions

--- run_raindrops.py
+++ run_raindrops.py
@@ -1,13 +1,15 @@
+## This function is used to check if the given number is divisable by a modulus and will return true or false
 def check_num(n, mod):
     return n % mod == 0
 
+# This function is used to print out all the factors of the given number
 def print_all_factors(n):
    print("The factors of",n,"are:")
    for i in range(1, n + 1):
        if n % i == 0:
            print(i)
 
-
+# This function will determine if the given number outputs Pling Plang or Plong and the factors of that number
 def pling_plang_plong(n):
     print_all_factors(n)
     if check_num(n, 5) and check_num(n, 3) and check_num(n, 7):
---
+++ test.py
@@ -0,0 +1,11 @@
+from run_raindrops import *
+
+print("Should return true if the arguments return pling")
+print(check_num(10, 5) == 'pling')
+print('got:', check_num(10,5))
+print("Should return true if the arguments return plang")
+print(check_num(9,3) == 'plang')
+print('got:', check_num(9,3))
+print("Should return true if the arguments return plong")
+print(check_num(20,5) == 'plong')
+print('got:', check_num(20, 5))
